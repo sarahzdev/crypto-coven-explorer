025834823bcbd2c739986d449512e1341e2f0443
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-04-06 21:14:01 -0700

    Create react app. Fetch some witches.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { createClient } from 'urql';
+import { useEffect, useState } from 'react';
+
+const API_URL = "https://api.thegraph.com/subgraphs/name/sazhang/sz-crypto-coven";
+
+const fillerQuery = `
+  query {
+    tokens(first: 6) {
+      id
+      tokenID
+      tokenURI
+      externalURL
+      image 
+      name 
+      description
+      type 
+      sun 
+      moon 
+      rising
+    }
+  }
+`;
+
+const client = createClient({
+  url: API_URL
+});
 
 function App() {
+  const [tokens, setTokens] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const response = await client.query(fillerQuery).toPromise();
+    setTokens(response.data.tokens);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="h1">Find your coven</h1>
+        <div className="card-wrapper">
+          {
+            tokens.map((token, index) => (
+              <div key={token.id} className="card">
+                <img className="card__image" src={token.image} alt={token.name}></img>
+                <h2>{token.name}</h2>
+                <p>{token.description}</p>
+                <div className="card__signs">
+                  <span>{token.sun}</span>
+                  &nbsp;&middot;&nbsp;
+                  <span>{token.moon}</span>
+                  &nbsp;&middot;&nbsp;
+                  <span>{token.rising}</span>
+                </div>
+                <a href={token.externalURL}>Learn more</a>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
